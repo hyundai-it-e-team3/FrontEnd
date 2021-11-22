5e55e55d9ddaf8f9d9f20632730e1737933a85c4
Chenjae <injea95@naver.com>
2021-11-22 09:23:57 +0900

    HYUN-43 <Front-End 회원 등급 정적 페이지>

---
+++ src/views/member/MemberLevel/index.vue
@@ -0,0 +1,37 @@
+<!-- 컴포넌트 UI 정의, root element가 하나만 존재해야한다 -->
+<template>
+    <v-card>
+        <v-expansion-panels accordion>
+            <level-component/>
+            <level-component/>
+            <level-component/>
+            <level-component/>
+            <level-component/>
+        </v-expansion-panels>
+    </v-card>  
+</template>
+
+<script>
+import LevelComponent from './levelComponent.vue';
+export default {
+    //컴포넌트의 대표이름 (devtools에 나오는 이름)
+    name: "",
+    //추가하고 싶은 컴포넌트 등록
+    components: {
+        LevelComponent
+    },
+    //컴포넌트 데이터 정의
+    data: function() {
+        return {
+        };
+    },
+    //컴포넌트 메소드 정의
+    methods: {
+    }
+}
+</script>
+
+<!-- 컴포넌트 스타일 정의 -->
+<style scoped>
+
+</style>
