8cc77099549ff9f9f197475bb94cc52117fde8a8
kim jungwoo <kjw970103@gmail.com>
2021-11-22 09:46:22 +0900

    HYUN-34 <FRONT-END 상품목록 정적페이지>

--- src/router/product.js
+++ src/router/product.js
@@ -31,4 +31,12 @@ export default[
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "product" */ '../views/product/ProductDetail')
   },
+  {
+    path: '/product/productList',
+    name: 'product_productlist',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "product" */ '../views/product/ProductList')
+  },
 ]
