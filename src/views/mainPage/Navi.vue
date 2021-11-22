e673dbd7566df3f1ce4ad38b3ee877e848d864f2
YENA <dpsk331@naver.com>
2021-11-22 10:26:48 +0900

    HYUN-29 <Front-End 베스트 정적 페이지>

--- src/views/mainPage/Navi.vue
+++ src/views/mainPage/Navi.vue
@@ -1,8 +1,8 @@
 <template>
   <v-col absolut class="pa-0">
         <v-btn to="/">홈</v-btn>
-        <v-btn to="mainpage/newitem">신상품</v-btn>
-        <v-btn>베스트</v-btn>
+        <v-btn to="/mainpage/newitem">신상품</v-btn>
+        <v-btn to="/mainpage/bestitem">베스트</v-btn>
         <v-btn>이벤트</v-btn>
   </v-col>
 </template>
---
+++ src/views/mainPage/bestItem/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container>
+    <v-col class="pa-1">
+      <navi />
+    </v-col>
+
+    <v-col class="mt-1">
+      <v-row>
+        <v-col cols="6" class="pa-0">
+            <product-card/>
+        </v-col>
+        <v-col cols="6" class="pa-0">
+            <product-card/>
+        </v-col>
+      </v-row>
+    </v-col>
+    <v-col>
+      <v-row>
+        <v-col cols="6" class="pa-0">
+            <product-card/>
+        </v-col>
+        <v-col cols="6" class="pa-0">
+            <product-card/>
+        </v-col>
+      </v-row>
+    </v-col>
+    <v-col>
+      <v-row>
+        <v-col cols="6" class="pa-0">
+            <product-card/>
+        </v-col>
+        <v-col cols="6" class="pa-0">
+            <product-card/>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-container>
+</template>
+
+<script>
+import Navi from '../Navi.vue';
+import ProductCard from '../ProductCard.vue';
+
+export default {
+  name:"bestItem",
+  components: {
+    Navi,
+    ProductCard
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
