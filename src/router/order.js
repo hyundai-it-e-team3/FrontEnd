e0bd0918a5e4d35538910accc3866ac766b4950e
Chenjae <injea95@naver.com>
2021-11-22 14:22:49 +0900

    HYUN-55 <Front-End 주문 목록 정적 페이지>

--- src/router/order.js
+++ src/router/order.js
@@ -4,4 +4,9 @@ export default[
     //lazyloding
     component: () => import(/* webpackChunkName: "about" */ '../views/order/Complete')
   },
+  {
+    path: "/order/orderlist",
+    //lazyloding
+    component: () => import(/* webpackChunkName: "about" */ '../views/order/OrderList')
+  },
 ]
