d762bcf15ddb60040f22963101af54b677990f8b
YENA <dpsk331@naver.com>
2021-11-29 00:23:42 +0900

    HYUN-111 <Front 로그인 페이지>

--- src/views/member/Login.vue
+++ src/views/member/Login.vue
@@ -1,60 +1,67 @@
-<!-- 컴포넌트 UI 정의, root element가 하나만 존재해야한다 -->
 <template>
-    <v-card>
-      <v-card-title>로그인</v-card-title>
-      <v-divider/>
-      <v-card-text>
-          <v-text-field
-        v-model="id"
-        :counter="10"
-        :rules="nameRules"
-        label="ID"
-        required
-        ></v-text-field>
-
-        <v-text-field
-        v-model="password"
-        :counter="10"
-        :rules="nameRules"
-        label="비밀번호"
-        required
-        ></v-text-field>
-
-        <v-row
-        class="justify-space-around mt-2"
-
-        >
-        <v-btn
-            color="black"
-            dark
-            width=auto
-        >로그인</v-btn>
-        <img src="@/assets/images/kakao_login.png" height="38px"/>
-        </v-row>
-        
-      </v-card-text>
-    </v-card>  
+  <v-container>
+    <v-row>
+        <v-col class="logo text-center">HANSOME</v-col>
+    </v-row>
+    <v-row>
+      <v-col class="col-12 pl-5 pr-5 pb-0">
+        <v-text-field label="아이디 입력"
+                      v-model="memberId"
+                      :rules="[rules.memberIdInput]"
+                      outlined></v-text-field>
+      </v-col>
+      <v-col class="col-12 pl-5 pr-5 pt-0">
+        <v-text-field label="비밀번호 입력"
+                      v-model="password"
+                      type="password"
+                      @click:append="passwordIconShow = !passwordIconShow"
+                      oninput="javascript: this.value = this.value.replace(/[\s]/g, '' );"
+                      :rules="[rules.passwordInput]"
+                      outlined>
+          <v-icon>mdi-eye</v-icon>
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <v-row class="justify-center ma-0">
+        <v-btn class="" width="165" depressed large color="black" dark>로그인</v-btn>
+        <v-btn class="ml-2" width="165" depressed large color="#fae100">카카오로 로그인</v-btn>
+    </v-row>
+    <hr class="ma-4" />
+    <v-row class="justify-center border">
+        <v-btn class="grey--text" plain to="/member/join">회원가입</v-btn>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-    //컴포넌트의 대표이름 (devtools에 나오는 이름)
-    name: "login",
-    //추가하고 싶은 컴포넌트 등록
-    components: {
-    },
-    //컴포넌트 데이터 정의
-    data: function() {
-        return {
-        };
-    },
-    //컴포넌트 메소드 정의
-    methods: {
-    }
-}
+  name: "Login",
+  components: {},
+  data: function () {
+    return {
+        memberId:  '',
+        password:  '',
+        rules: {
+            memberIdInput: value => !!value  || '아이디를 입력하세요.',
+            passwordInput: value => !!value  || '비밀번호를 입력하세요.'
+        }
+    };
+  },
+  methods: {
+  },
+};
 </script>
 
-<!-- 컴포넌트 스타일 정의 -->
 <style scoped>
-
+.logo {
+    font-size: 2.5em;
+    font-weight: 500;
+}
+.v-text-field >>> input {
+    font-size: 1.2em;
+    font-weight: 400;
+}
+.v-btn {
+    font-size: 3em;
+}
 </style>
