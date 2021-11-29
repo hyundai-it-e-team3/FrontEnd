0424835d674760f1e80a2ce75310a6aa99379420
Chenjae <injea95@naver.com>
2021-11-29 19:45:05 +0900

    HYUN-166 <주문 목록 조회 API 연결>

--- src/views/order/OrderList/OrderComponent.vue
+++ src/views/order/OrderList/OrderComponent.vue
@@ -2,18 +2,35 @@
 <template>
     <v-card>
       <v-card-title @click="goDetail">
-          2014503219232
+          No.{{orderId}}
       </v-card-title>
       <v-card-subtitle>
-          20121.11.11 12:32:20
+          주문 일시 : {{orderDate}}
       </v-card-subtitle>
+      <v-card-text>
+          총 가격 : {{totalPrice}} / 주문 상태 : {{state}}
+      </v-card-text>
       <v-divider/>
-      <product-component/>
-      <product-component/>
+      <v-row v-for="(product) in productList" :key="product.productDetailId">
+        <product-component
+                :product = product
+        />
+      </v-row>
+      <v-card-actions>
+          <v-btn>배송지 수정</v-btn>
+          <v-btn>주문 확정</v-btn>
+          <v-btn>환불</v-btn>
+          <v-btn>반품/교환 신청</v-btn>
+      </v-card-actions>
+        <alert-dialog v-if="alertDialog"
+            :loading="loading"
+            :message="alertDialogMessage"
+            @close="alertDialog=false"/>
     </v-card>  
 </template>
 
 <script>
+import orderAPI from "@/apis/order";
 import ProductComponent from './ProductComponent.vue';
 export default {
     //컴포넌트의 대표이름 (devtools에 나오는 이름)
@@ -25,6 +42,10 @@ export default {
     //컴포넌트 데이터 정의
     data: function() {
         return {
+            loading: false,
+            alertDialog: false,
+            alertDialogMessage: null,
+            productList: null
         };
     },
     //컴포넌트 메소드 정의
@@ -32,6 +53,37 @@ export default {
         goDetail(){
             this.$router.push("/order/complete");
         },
+        handleOrderProductList() {
+            this.loading = true;
+            this.alertDialog = true;
+            orderAPI.getOrderDetailList(this.orderId)
+                .then(response => {
+                console.log(response.data);
+                this.productList = response.data;
+                this.loading = false;
+                this.alertDialog = false;
+                }).catch(error => {
+                if(error.response) {
+                    if(error.response.status === 403) {
+                        this.loading = false;
+                        this.alertDialog = false;
+                        this.$router.push("/menu07/auth/jwtauth")
+                    }
+                } else {
+                    this.loading = false;
+                    this.alertDialogMessage = "네트워크 통신 오류";
+                }
+            });
+        }
+    },
+    props: [
+        "orderId",
+        "orderDate",
+        "totalPrice",
+        "state"
+    ],
+    created() {
+         this.handleOrderProductList();
     }
 }
 </script>
